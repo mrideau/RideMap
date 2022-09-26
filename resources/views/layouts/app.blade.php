28897db54d9de8250c15055716f972f3d0823407
mrideau <matis.rideau@hotmail.com>
2022-09-26 14:20:10 +0200

    :zap: Font preloading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,8 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@1,700&family=Barlow+Condensed:wght@500;700&family=Rubik&display=swap" rel="stylesheet">
+    <link rel="preload" as="font" href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@1,700&family=Barlow+Condensed:wght@500;700&family=Rubik&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@1,700&family=Barlow+Condensed:wght@500;700&family=Rubik&display=swap">
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
